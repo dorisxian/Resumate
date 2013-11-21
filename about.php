aa7df32f94189cac01a0c2dd0b116fce8e7f55d0
swimaster100 <vastam@rpi.edu>
2013-11-21 16:30:42 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+	<title>About | Resumate</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lobster+Two|Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<?php include("header.php"); ?>
+	<div id="container">
+		<div id="about">
+			<h2>About Resumate</h2>
+			<p> Test By Madi. Lorem ipsum eu aute irure in voluptate amet dolor ullamco amet est reprehenderit amet tempor eiusmod id minim officia aliquip reprehenderit irure aliquip incididunt do eu occaecat officia. Lorem ipsum eu aute irure in voluptate amet dolor ullamco amet est reprehenderit amet tempor eiusmod id minim officia aliquip reprehenderit irure aliquip incididunt do eu occaecat officia. Lorem ipsum eu aute irure in voluptate amet dolor ullamco amet est reprehenderit amet tempor eiusmod id minim officia aliquip reprehenderit irure aliquip incididunt do eu occaecat officia. </p>
+			<p>Lorem ipsum anim esse do mollit aute incididunt culpa non enim quis magna ad nulla consequat id qui non laborum dolore eu reprehenderit labore cupidatat qui fugiat irure sunt consequat dolore ea incididunt ad do in incididunt occaecat cillum id fugiat ut aute irure sunt ullamco adipisicing sed ex mollit est ea nostrud proident non ex dolor Duis nisi ullamco incididunt sit ut sit cupidatat nostrud magna sit incididunt elit voluptate anim in ut magna sunt do proident reprehenderit sint est non consequat in consequat commodo nulla dolore reprehenderit consequat elit eu nulla nisi enim reprehenderit in deserunt elit dolore sint do dolore cupidatat labore exercitation ut Duis mollit commodo do aute pariatur mollit nostrud dolore sunt fugiat non amet nostrud labore dolor minim anim do non officia enim exercitation est cupidatat dolor non deserunt do aliquip est occaecat qui Excepteur deserunt dolor nisi et in amet nostrud est minim aliqua ut dolore dolore ad ullamco eiusmod nostrud veniam nostrud velit occaecat aliqua fugiat sunt commodo voluptate laboris incididunt et do voluptate proident nostrud consequat dolore in in do do aute labore ut velit aute consequat exercitation minim magna nisi labore elit dolor aute veniam sint quis qui commodo anim ullamco in magna in Ut cillum in aliquip labore magna cillum mollit in ex Ut tempor aliqua et quis enim.</p>
+		</div>
+	</div>
+	<footer>
+		Copyright &copy; Resumate
+	</footer>
+
+	<!-- Scripts !-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/scripts.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
