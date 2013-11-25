ca6606960b2a0dd24d03b464fb63567bdc50c3df
Brolita <Byrnec3@rpi.edu>
2013-11-25 16:55:10 -0500

    Updated resume.php

--- php/resume.php
+++ php/resume.php
@@ -21,37 +21,39 @@
 	<body>
 		<section id="header">
 			<ul id="basicInfo">
-				<li id="firstName"><?php echo load['fname'][0]; ?></li>
-				<li id="lastName"<?php echo load['lname'][0]; ?>></li>
-				<li id="phone"><?php echo load['phone'][0]; ?></li>
-				<li id="email"><?php echo load['email'][0]; ?></li>
+				<?php if(isset(load['fname'])): ?>		<li id="firstName"><?php echo load['fname'][0]; ?></li>			<?php endif; ?>
+				<?php if(isset(load['lname'])): ?>		<li id="lastName"<?php echo load['lname'][0]; ?>></li>			<?php endif; ?>
+				<?php if(isset(load['phone'])): ?>		<li id="phone"><?php echo load['phone'][0]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['email'])): ?>		<li id="email"><?php echo load['email'][0]; ?></li>				<?php endif; ?>
 			</ul>
 			<ul id="addressInfo">
-				<li id="addressLine1"><?php echo load['address1'][0]; ?></li>
-				<li id="addressLine2"><?php echo load['address2'][0]; ?></li>
-				<li id="city"><?php echo load['city'][0]; ?></li>
-				<li id="state"><?php echo load['state'][0]; ?></li>
-				<li id="zip"><?php echo load['zip'][0]; ?></li>
+				<?php if(isset(load['address1'])): ?>	<li id="addressLine1"><?php echo load['address1'][0]; ?></li>	<?php endif; ?>
+				<?php if(isset(load['address2'])): ?>	<li id="addressLine2"><?php echo load['address2'][0]; ?></li>	<?php endif; ?>
+				<?php if(isset(load['city'])): ?>		<li id="city"><?php echo load['city'][0]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['state'])): ?>		<li id="state"><?php echo load['state'][0]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['zip'])): ?>		<li id="zip"><?php echo load['zip'][0]; ?></li>					<?php endif; ?>
 			</ul>
+			<?php if(isset(load['website'])): ?>
 			<ul id="additionalInfo">
 				<li id="website"><?php echo load['website'][0]; ?></li>
 			</ul>
+			<?php endif; ?>
 		</section>
 		<section class="body" id="objective">
-			<div id="statement"><?php echo load['obj'][0]; ?></div>
-			<div id="profile"><?php echo load['profile'][0]; ?></div>
+			<?php if(isset(load['obj'])): ?>		<div id="statement"><?php echo load['obj'][0]; ?></div> 	<?php endif; ?>
+			<?php if(isset(load['profile'])): ?>	<div id="profile"><?php echo load['profile'][0]; ?></div> 	<?php endif; ?>
 		</section>
 		<section class="body" id="education">
 			<?php foreach($_POST['schoolname'] as $i=>$value): ?>
 			<ul class="school">
-				<li class="sName"><?php echo load['schoolname'][$i]; ?></li>
-				<li class="sCity"><?php echo load['schoolcity'][$i]; ?></li>
-				<li class="sContry"><?php echo load['schoolcontry'][$i]; ?></li>
-				<li class="sStartDate"><?php echo load['schoolstartdate'][$i]; ?></li>
-				<li class="sEndDate"><?php echo load['schoolenddate'][$i]; ?></li>
-				<li class="sMajor"><?php echo load['maj'][$i]; ?></li>
-				<li class="sMinor"><?php echo load['min'][$i]; ?></li>
-				<li class="sGPA"><?php echo load['gpa'][$i]; ?></li>
+				<?php if(isset(load['schoolname'][$i])): ?>		<li class="sName"><?php echo load['schoolname'][$i]; ?></li>			<?php endif; ?>
+				<?php if(isset(load['schoolcity'][$i])): ?>		<li class="sCity"><?php echo load['schoolcity'][$i]; ?></li>			<?php endif; ?>
+				<?php if(isset(load['schoolcontry'][$i])): ?>	<li class="sContry"><?php echo load['schoolcontry'][$i]; ?></li>		<?php endif; ?>
+				<?php if(isset(load['schoolstartdate'][$i])): ?><li class="sStartDate"><?php echo load['schoolstartdate'][$i]; ?></li>	<?php endif; ?>
+				<?php if(isset(load['schoolenddate'][$i])): ?>	<li class="sEndDate"><?php echo load['schoolenddate'][$i]; ?></li>		<?php endif; ?>
+				<?php if(isset(load['maj'][$i])): ?>			<li class="sMajor"><?php echo load['maj'][$i]; ?></li>					<?php endif; ?>
+				<?php if(isset(load['min'][$i])): ?>			<li class="sMinor"><?php echo load['min'][$i]; ?></li>					<?php endif; ?>
+				<?php if(isset(load['gpa'][$i])): ?>			<li class="sGPA"><?php echo load['gpa'][$i]; ?></li>					<?php endif; ?>
 			</ul>
 			<?php endforeach; ?>
 			<ul id="relevantCourseWork">
@@ -65,26 +67,26 @@
 			<?php foreach ($_POST['type'] as $i=>$value):; ?>
 			<?php if($_POST['workhere'][$i] == 'workhere'):; ?>
 			<ul class="job" id="currentJob">
-				<li class="wCompany"><?php echo load['type'][$i]; ?></li>
-				<li class="wTitle"><?php echo load['position'][$i]; ?></li>
-				<li class="wStartDate"><?php echo load['workstatedate'][$i];; ?></li>
-				<li class="wCity"><?php echo load['workcity'][$i]; ?></li>
-				<li class="wDescription"><?php echo load['workdescription'][$i]; ?></li>
+				<?php if(isset(load['type'][$i])): ?>			<li class="wCompany"><?php echo load['type'][$i]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['position'][$i])): ?>		<li class="wTitle"><?php echo load['position'][$i]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['workstartdate'][$i])): ?>	<li class="wStartDate"><?php echo load['workstatedate'][$i];; ?></li>	<?php endif; ?>
+				<?php if(isset(load['workcity'][$i])): ?>		<li class="wCity"><?php echo load['workcity'][$i]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['workdescription'][$i])): ?><li class="wDescription"><?php echo load['workdescription'][$i]; ?></li><?php endif; ?>
 			</ul>
 			<?php else: ?>
 			<ul class="job">
-				<li class="wCompany"><?php echo load['type'][$i]; ?></li>
-				<li class="wTitle"><?php echo load['position'][$i]; ?></li>
-				<li class="wStartDate"><?php echo load['workstatedate'][$i]; ?></li>
-				<li class="wEndDate"><?php echo load['workenddate'][$i]; ?></li>
-				<li class="wCity"><?php echo load['workcity'][$i]; ?></li>
-				<li class="wDescription"><?php echo load['workdescription'][$i]; ?></li>
+				<?php if(isset(load['type'][$i])): ?>			<li class="wCompany"><?php echo load['type'][$i]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['position'][$i])): ?>		<li class="wTitle"><?php echo load['position'][$i]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['workstartdate'][$i])): ?>	<li class="wStartDate"><?php echo load['workstatedate'][$i];; ?></li>	<?php endif; ?>
+				<?php if(isset(load['workenddate'][$i])): ?>	<li class="wEndDate"><?php echo load['workenddate'][$i]; ?></li>		<?php endif; ?>
+				<?php if(isset(load['workcity'][$i])): ?>		<li class="wCity"><?php echo load['workcity'][$i]; ?></li>				<?php endif; ?>
+				<?php if(isset(load['workdescription'][$i])): ?><li class="wDescription"><?php echo load['workdescription'][$i]; ?></li><?php endif; ?>
 			</ul>
 			<?php endif; ?>
 			<?php endforeach; ?>
 		</section>
 		<section class="body" id="skills">
 			??? doesn't look complete to me
-		</section>
+		</section>	
 	</body>
 </html>
