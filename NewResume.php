3525e93a86d2d711345b069af42fded4c57cae63
Brolita <Byrnec3@rpi.edu>
2013-12-03 16:52:07 -0500

    some bug fix

--- NewResume.php
+++ NewResume.php
@@ -2,7 +2,7 @@
 	session_start();
 	if(isset($_GET['num'])) {
 		try {
-			include('connect.php');
+			include('php/connect.php');
 			// $dbname = 'resumate';
 			// $user = 'root';
 			// $pass = '';
@@ -89,8 +89,8 @@
 					<p><label>Profile</label><textarea id="profile" name="profile" placeholder="Enter profile"					value="<?php echo $load->profile ?>"></textarea></p>	
 				</div>
 				<div id="education-form" class="formblock hidden">
+					<?php for($i = 0;  $i < $load->schoolname->count(); $i++): ?>
 					<div class="ed_fields">
-						<?php foreach($load->schoolname as $i=>$value): ?>
 						<p><label>Name</label><input type="text" name="schoolname[]" placeholder="Enter name of school"			value="<?php echo $load->schoolname[$i] ?>"></p>
 						<p><label>City</label><input type="text" name="schoolcity[]" placeholder="Enter the city of school"		value="<?php echo $load->schoolcity[$i] ?>"></p>
 						<p>
@@ -107,15 +107,15 @@
 						<p><label>Major</label><input type="text" name="maj[]" placeholder="Enter major if applicable"	value="<?php echo $load->maj[$i] ?>"></p>
 						<p><label>Minor</label><input type="text" name="min[]" placeholder="Enter minor if applicable"	value="<?php echo $load->min[$i] ?>"></p>
 						<p><label>GPA</label><input type="text" name="gpa[]" placeholder="Enter GPA"					value="<?php echo $load->gpa[$i] ?>"></p>
-						<?php endfor; ?>
 					</div>
+					<?php endfor; ?>
 					<p class="button" id="deleteSchool"><i class="fa fa-times fa-fw"></i>Delete Entry</p>
 					<p class="button" id="addSchool"><i class="fa fa-plus fa-fw"></i>Add Entry</p>
 					
 				</div>
 				<div id="work-form" class="formblock hidden">	
+					<?php for($i = 0;  $i < $load->type->count(); $i++): ?>
 					<div class="w_fields">
-						<?php for($i = 0;  $i < $load->type->count(); $i++): ?>
 						<p><label>Company</label><input type="text" name="type[]" placeholder="Enter Company Name"									value="<?php echo $load->type[$i] ?>"></p>
 						<p><label>Job Title</label><input type="text" name="position[]" placeholder="Enter position held"							value="<?php echo $load->position[$i] ?>"></p>
 						<p><label>Start Date</label><input type="text" name="workstartdate[]" class="startDatepicker" placeholder="Enter start date"	value="<?php echo $load->workstartdate[$i] ?>"/></p>
@@ -124,8 +124,8 @@
 						<p><label>Description</label><input type="textarea" name="workdescription[]" placeholder="Enter the job description"		><?php echo $load->workdescription[$i] ?></textarea></p>
 						<p class="check"><label>I currently work here</label><input type="checkbox"></p>
 						<!-- <input type="radio" name="workhere" value="1" />I currently work here -->
-						<?php endfor; ?>
 					</div>
+					<?php endfor; ?>
 					<p class="button" id="deleteWork"><i class="fa fa-times fa-fw"></i>Delete Entry</p>
 					<p class="button" id="addWork"><i class="fa fa-plus fa-fw"></i>Add Entry</p>
 				</div>
